7eb231554a6f349d251ae8e3dbf7acba9e44014f
bang <daiyuhang417@gmail.com>
2019-08-07 10:27:04 +0800

    first commit

---
+++ part10-cloud/src/main/java/com/aotain/Part10CloudApplication.java
@@ -0,0 +1,13 @@
+package com.aotain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part10CloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part10CloudApplication.class, args);
+	}
+
+}
---
+++ part9-springboot/src/main/java/com/aotain/Part9SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.aotain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part9SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part9SpringbootApplication.class, args);
+	}
+
+}
