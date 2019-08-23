3a71e203235a11888d56d0f28915d4e87fdef647
bang <daiyuhang417@gmail.com>
2019-08-23 13:42:36 +0800

    add jframe alarm

---
+++ part1-basic/src/main/java/com/aotain/jwt/Main.java
@@ -0,0 +1,17 @@
+package com.aotain.jwt;
+
+/**
+ * Demo class
+ *
+ * @author daiyh@aotain.com
+ * @date 2019/08/15
+ */
+public class Main {
+
+    private final static String TITLE="弹窗";
+
+    public static void main(String[] args) {
+        FrameUtil test = new FrameUtil();
+        test.show(TITLE, "这是一个弹窗测试！");
+    }
+}
