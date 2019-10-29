043eb7e31105fca59a2284698b1dcf28e13b45d0
bang <daiyuhang417@gmail.com>
2019-10-29 16:58:36 +0800

    xx

--- part1-basic/src/main/java/com/aotain/string/StringMatter.java
+++ part1-basic/src/main/java/com/aotain/string/StringMatter.java
@@ -13,5 +13,18 @@ public class StringMatter {
 
         System.out.println(str1.intern()==str1);
         System.out.println(str2.intern()==str2);
+
+        System.out.println("======================");
+        String s3 = "abcd";
+        String s4 = "a"+"b"+"c"+"d";
+        String s5 = new String("abcd");
+        String s6 = new String("abcd").intern();
+        // true
+        System.out.println(s3==s4);
+        // false
+        System.out.println(s3==s5);
+        // true
+        System.out.println(s3==s6);
+
     }
 }
