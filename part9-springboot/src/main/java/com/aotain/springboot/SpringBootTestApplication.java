b5fda2e1f5a810ada26f24655db80cc76e5fef36
bang <daiyuhang417@gmail.com>
2019-10-29 18:12:10 +0800

    commit files

---
+++ part9-springboot/src/main/java/com/aotain/springboot/SpringBootTestApplication.java
@@ -0,0 +1,17 @@
+package com.aotain.springboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Demo class
+ *
+ * @author daiyh@aotain.com
+ * @date 2019/10/29
+ */
+@SpringBootApplication
+public class SpringBootTestApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
